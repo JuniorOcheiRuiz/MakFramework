a272c9024d2868fb495a198f159b108cc7ce30d7
Junior Ochei Ruiz <mak.hmc@gmail.com>
2017-10-08 20:42:34 +0200

    Developing Router.php

--- src/Routing/Interfaces/RouterInterface.php
+++ src/Routing/Interfaces/RouterInterface.php
@@ -8,6 +8,7 @@ interface RouterInterface
 {
   /**
    * map
+   *
    * @param string[] $methods
    * @param string $pattern
    * @param string|callable|Closure $callback
@@ -17,6 +18,7 @@ interface RouterInterface
 
   /**
    * get
+   *
    * @param string $pattern
    * @param string|callable|Closure $callback
    * @return RouteInterface
@@ -24,13 +26,107 @@ interface RouterInterface
   public function get(string $pattern, $callback) : RouteInterface;
 
   /**
+   * post
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function post(string $pattern, $callback) : RouteInterface;
+
+  /**
+   * put
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function put(string $pattern, $callback) : RouteInterface;
+
+  /**
+   * delete
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function delete(string $pattern, $callback) : RouteInterface;
+
+  /**
+   * options
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function options(string $pattern, $callback) : RouteInterface;
+
+  /**
+   * patch
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function patch(string $pattern, $callback) : RouteInterface;
+
+  /**
+   * any
+   *
+   * @param string $pattern
+   * @param string|callable|Closure $callback
+   * @return RouteInterface
+   */
+  public function any(string $pattern, $callback) : RouteInterface;
+
+  /**
    * group
+   *
    * @param string $pattern
    * @param callable|Closure $callback
    * @return RouteGroupInterface
    */
   public function group(string $pattern, $callback) : RouteGroupInterface;
 
+  /**
+   * getRoute
+   *
+   * @param string $pattern
+   * @return RouteInterface
+   */
+  public function getRoute(string $pattern) : RouteInterface;
+
+  /**
+   * getRoutes
+   *
+   * @return RouteInterface[]
+   */
+  public function getRoutes() : array;
+
+  /**
+   * setRoute
+   *
+   * @param array $methods
+   * @param string $pattern
+   *
+   * @return RouterInterface
+   */
+  public function setRoute() : RouterInterface;
+
+  /**
+   * setRoutes
+   *
+   * @param RouteInterface[]
+   * @return RouterInterface
+   */
+  public function setRoutes(array $routes) : RouterInterface;
+
+
+  /**
+   * run
+   *
+   * @return void
+   */
   public function run() : void;
 
 }
--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -1,6 +1,7 @@
 <?php
 namespace Makframework\Routing;
 
+use Makframework\Routing\Interfaces\RouteInterface;
 use Makframework\Routing\Interfaces\RouterInterface;
 
 /**
@@ -9,9 +10,10 @@ use Makframework\Routing\Interfaces\RouterInterface;
 class Router implements RouterInterface
 {
   /**
-   *  @var Route[]
+   *  @var RouteInterface[]
    */
   protected $routes = [];
+
 
   protected $container;
 
@@ -25,7 +27,22 @@ class Router implements RouterInterface
 
   }
 
+  protected function builtSegments(string $pattern) : array 
+  {
+
+  }
+
+  protected function match(string $requestPath) : RouteInterface
+  {
+    foreach ($this->routes as $pattern => $route) {
+      if (preg_match('#')) {
+
+      }
+    }
+
+    return false;
+  }
 
   public function run() : void;
-  
+
 }
